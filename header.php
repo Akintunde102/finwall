cddea36e973561f4711055fc4443e090b8dc4a66
Akintunde102 <jegedeakintunde@gmail.com>
2017-12-28 15:34:19 +0100

    Update header.php

--- header.php
+++ header.php
@@ -16,8 +16,8 @@ $objYahooStock = new YahooStock;
 
     <script type="text/javascript" src="http://www.google.com/jsapi"></script>
 
-    <script src="js/jquery/jquery-1.8.2.min.js" type="text/javascript" ></script>
-    <link href="css/customize-template.css" type="text/css" media="screen, projection" rel="stylesheet" />
+    <script src="assets/plugins/jquery-2.0.3.min.js" type="text/javascript" ></script>
+    <link href="less/customize-template.less" type="text/css" media="screen, projection" rel="stylesheet" />
 
     <style>
     </style>
